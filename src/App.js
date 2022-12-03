6da8820973f08432eb41ff39abd676c8851c3d95
Nathan Cunningham <counterweight22@gmail.com>
2022-12-03 11:35:39 -0800

    Added Header and Footer components. Added Campsite Cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import { CAMPSITES } from './app/shared/CAMPSITES';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
